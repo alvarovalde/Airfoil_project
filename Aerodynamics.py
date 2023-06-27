835d58202a9660988dc6c1d51a8b4848c5ea4db2
alvarovalde <alvarovalde03@gmail.com>
2023-06-27 17:59:15 +0200

    work on Aerodynamics: code readability

--- Aerodynamics.py
+++ Aerodynamics.py
@@ -1,48 +1,70 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Qt5Agg')
-class Space_domain:
+
+
+class SpaceDomain:
     """
     This class creates a virtual 2D space to plot areodynamics,
     it is based on linear, non-viscous, airfoil theory.
     you can create freestreams, sources,sinks, doublets and vortexes and plot them.
     """
-    def __init__(self,num_of_points_per_direction=200):
+    def __init__(self, num_of_points_per_direction=200):
+        # grid parameters
         self.N = num_of_points_per_direction    # resolution of grid will be (N,N). Unstable below N = 50
         self.X = None                           # X position of points in grid: (N,N) array of x coordinates
-        self.Y =None                            # Y position of points in grid: (N,N) array of y coordinates
+        self.Y = None                            # Y position of points in grid: (N,N) array of y coordinates
         self.u = None                           # X comp of speed vector grid: (N,N) array of u component
         self.v = None                           # Y comp of speed vector grid: (N,N) array of v component
-        self.u_inf =None                        # freestream speed, float
         self.cp = None                          # pressure coefficient: (N,N) array of C_p at every point
+        self.psi = 0                            # streamline function,  (N,N) array of values
+
+        # flow parameters and Points Of Interest
+        self.u_inf = None                       # freestream speed, float
         self.s_s_pos = []                       # positions of sources,sinks,... , list of tuples
-        self.psi = 0                            #
+        self.st_points = []                     # positions of stagnation points, list of tuples
 
-        #plotting parameters
-        self.x_start = -2                       #
-        self.x_end = 2                          #
-        self.y_start = -1                       #
-        self.y_end = 1                          #
-
+        # plotting parameters
+        self.x_start = -5                       # start of view window in x direction
+        self.x_end = 5                          # end of view window in x direction
+        self.y_start = -5                       # start of view window in y direction
+        self.y_end = 5                          # end of view window in y direction
 
     def create_grid(self):
-        x = np.linspace(self.x_start - 1,self.x_end +11,self.N)
-        y = np.linspace(self.y_start - 1,self.y_end +1,self.N)
-        self.X,self.Y = np.meshgrid(x,y)
-        self.u = np.zeros_like(self.X,dtype=float)
-        self.v = np.zeros_like(self.Y,dtype=float)
+        """
+        This function creates the grid first generating a linear space (N points),
+        and then creates a mesh of these points (N,N) ands divides it into its
+         X and Y coordinates. (+/- 1 for plotting neatness). Initializes the shape of u and v.
+        """
+        x = np.linspace(self.x_start - 1, self.x_end + 1, self.N)
+        y = np.linspace(self.y_start - 1, self.y_end + 1, self.N)
+        self.X, self.Y = np.meshgrid(x, y)
+        self.u = np.zeros_like(self.X, dtype=float)
+        self.v = np.zeros_like(self.Y, dtype=float)
 
-    def create_freestream(self, freestream_speed=1.0,angle=0.0):
+    def create_freestream(self, freestream_speed=1.0, angle=0.0):
+        """
+        Creates linear flow behaviour and ads it to current flow state.
+
+        @param freestream_speed: the speed of the created flow [-]
+        @param angle: angle of attack, degrees.
+        """
+
         self.u_inf = freestream_speed
-        self._inf = self.u_inf * np.ones(self.X.shape,dtype=float)
-        a = angle * np.pi /180
+        a = angle * np.pi / 180
         self.psi = self.psi + self.u_inf * self.Y
         self.u = self.u + self.u_inf*np.cos(a)
         self.v = self.v + self.u_inf * np.sin(a)
 
     def create_source_flow(self,strength=1.0,x_sr= 0.0,y_sr= 0.0):
+        """
+        Creates a source behaviour and ads it to current flow state.
+
+        @param strength:
+        @param x_sr:
+        @param y_sr:
+        """
         # compute the velocity field on the mesh grid
         self.u = self.u + (strength/(2*np.pi))*((self.X -x_sr)/((self.X -x_sr)**2 + (self.Y -y_sr)**2))
         self.v = self.v + (strength / (2 * np.pi) *((self.Y - y_sr) / ((self.X - x_sr) ** 2 + (self.Y - y_sr) ** 2)))
@@ -57,21 +79,35 @@ class Space_domain:
         self.psi = self.psi + strength / (2 * np.pi) * np.arctan2((self.Y - y_snk), (self.X - x_snk))
 
     def create_vortex(self,gamma=5.0,x_vor=0.0,y_vor=0.0):
-        self.u = self.u + +gamma / (2 * math.pi) * (self.Y - y_vor) / ((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
-        self.v = self.v + -gamma / (2 * math.pi) * (self.X - x_vor) / ((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
-        self.psi = self.psi + gamma / (4 * math.pi) * np.log((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
+        self.gamma = gamma
+        self.u = self.u + +gamma / (2 * np.pi) * (self.Y - y_vor) / ((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
+        self.v = self.v + -gamma / (2 * np.pi) * (self.X - x_vor) / ((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
+        self.psi = self.psi + gamma / (4 * np.pi) * np.log((self.X - x_vor) ** 2 + (self.Y - y_vor) ** 2)
         self.s_s_pos.append((x_vor, y_vor))
 
 
     def create_doublet(self,kappa=1, xpos=0.0,ypos=0.0 ):
-        self.u = self.u +(-kappa / (2 * math.pi) *
+        self.u = self.u +(-kappa / (2 * np.pi) *
              ((self.X - xpos) ** 2 - (self.Y - ypos) ** 2) /
              ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2) ** 2)
-        self.v = self.v + (-kappa / (2 * math.pi) *
+        self.v = self.v + (-kappa / (2 * np.pi) *
              2 * (self.X - xpos) * (self.Y - ypos) /
              ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2) ** 2)
 
-        self.psi = -kappa / (2 * math.pi) * (self.Y - ypos) / ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2)
+        self.psi = -kappa / (2 * np.pi) * (self.Y - ypos) / ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2)
+        self.s_s_pos.append((xpos, ypos))
+        # calculate the cylinder radius
+        self.R = np.sqrt(kappa/(2*np.pi*self.u_inf))
+
+    def create_doublet_with_vortex(self,kappa=1,gamma=4, xpos=0.0,ypos=0.0 ):
+        self.u = self.u +(-kappa / (2 * np.pi) *
+             ((self.X - xpos) ** 2 - (self.Y - ypos) ** 2) /
+             ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2) ** 2)
+        self.v = self.v + (-kappa / (2 * np.pi) *
+             2 * (self.X - xpos) * (self.Y - ypos) /
+             ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2) ** 2)
+        self.create_vortex(gamma,xpos,ypos)
+        self.psi = -kappa / (2 * np.pi) * (self.Y - ypos) / ((self.X - xpos) ** 2 + (self.Y - ypos) ** 2)
         self.s_s_pos.append((xpos, ypos))
         # calculate the cylinder radius
         self.R = np.sqrt(kappa/(2*np.pi*self.u_inf))
@@ -82,21 +118,39 @@ class Space_domain:
         except TypeError as err:
             print('You need to create a freestream to get a Cp, try creating a uniform flow!',err)
             quit()
+    def get_stagnation_points(self):
+        try:
+            self.st_points.append((+np.sqrt(self.R**2 - (self.gamma / (4 * np.pi * self.u_inf))**2),-self.gamma / (4 * np.pi * self.u_inf)))
+            self.st_points.append((-np.sqrt(self.R**2 - (self.gamma / (4 * np.pi * self.u_inf))**2),-self.gamma / (4 * np.pi * self.u_inf)))
+            print(self.st_points)
+        except RuntimeWarning as err:
+            print('gamma is too large')
+            pass
 
     def plot_field(self):
-        ax = plt.subplot()
+        fig,ax = plt.subplots()
         ax.axis([self.x_start, self.x_end, self.y_start, self.y_end])
         ax.streamplot(self.X, self.Y, self.u, self.v, density=5, linewidth=1, arrowsize=1,arrowstyle='->')
         ax.contour(self.X,self.Y,self.psi,levels=[0.], colors='#CD2305', linewidths=2, linestyles='solid')
 
         circle = plt.Circle((0, 0), radius=self.R, color='#CD2305', alpha=0.5)
-        plt.gca().add_patch(circle)
+        ax.add_patch(circle)
+
+        # Try to plot the source,sink,... positions
         try:
             x,y = np.array(self.s_s_pos).T
             ax.scatter(x,y,color='red')
-            print(so.s_s_pos)
+            print(self.s_s_pos)
         except ValueError as err:
             print('No points to plot',err)
+
+        #try to plot the stagnation points
+        try:
+            x,y = np.array(self.st_points).T
+            ax.scatter(x,y,color='g')
+        except Error as err:
+            pass
+        plt.show()
 
 
 
@@ -112,12 +166,19 @@ class Space_domain:
         plt.show()
 
 if __name__ == '__main__':
-    so =Space_domain()
-    so.create_grid()
-    so.create_freestream(0.5,0)
-    #so.create_vortex()
-    so.create_doublet()
-    so.get_cp()
+    #create an instance of the Space_domain and generate the grid
+    sd =SpaceDomain()
+    sd.create_grid()
 
-    so.plot_field()
-    so.plot_cp()
+    #create a freestream
+    sd.create_freestream(0.5,0)
+
+    #create a doublet with a vortex
+    sd.create_doublet_with_vortex()
+
+
+    sd.get_stagnation_points()
+    sd.get_cp()
+    print('Hello',sd.R)
+    sd.plot_field()
+    #sd.plot_cp()
